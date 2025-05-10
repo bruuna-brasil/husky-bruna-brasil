75fbcda92de0bf9e509bf8c92b4646d9590d8d5e
bruuna-brasil <124185478+bruuna-brasil@users.noreply.github.com>
2025-05-09 22:32:42 -0300

    test: commitando

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: ["js/recommended"],
+  },
+]);
